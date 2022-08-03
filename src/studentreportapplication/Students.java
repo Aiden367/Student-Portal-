9601f03ba45cf9d43240ae932fa96d76bbbdfbe0
Aiden367 <aidenbyrne53@gmail.com>
2022-08-03 15:51:54 +0200

    Second Commit - Bug Fixes and adding Table feature

--- src/studentreportapplication/Students.java
+++ src/studentreportapplication/Students.java
@@ -1,18 +1,21 @@
-
 package studentreportapplication;
 
+import java.sql.Statement;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
-
-
+import java.sql.*;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 public class Students
 {
- DatabaseConnection dataBaseConnection = new DatabaseConnection();
-   private String lastName,firstName,studentClass,studentLocation;
-   private int studentGrade,studentAge,studentContactInformation;
+
+    DatabaseConnection dataBaseConnection = new DatabaseConnection();
+    private String lastName, firstName, studentClass, studentLocation;
+    private int studentGrade, studentAge, studentContactInformation;
+    private Object [][] studentData;
+    
     JTextField setLastName;
     JTextField setFirstName;
     JTextField setStudentAge;
@@ -20,136 +23,176 @@ public class Students
     JTextField setStudentGrade;
     JTextField setStudentLocation;
     JTextField setStudentContactInformation;
-   firstDisplay firstDisplay = new firstDisplay();
-   
+    
+//    firstDisplay firstDisplay = new firstDisplay();
+
+//----------------------Start of Getters and Setters--------------------------//
+    
+    
+    public void setStudentData(Object [][] learnerData)
+    {
+        this.studentData = learnerData;
+    }
+    
+    public Object[][] getStudentData()
+    {
+        return studentData;
+    }
+    
     public String getLastName()
     {
         return lastName;
     }
-    
+
     public void setLastName(String learnerLastName)
     {
         this.lastName = learnerLastName;
     }
-    
+
     public String getFirstName()
     {
-       return firstName;
+        return firstName;
     }
-    
+
     public void setFirstName(String learnerFirstName)
     {
         this.firstName = learnerFirstName;
     }
-    
+
     public String getStudentClass()
     {
         return studentClass;
     }
-    
+
     public void setStudentClass(String learnerClass)
     {
         this.studentClass = learnerClass;
     }
+
     public String getStudentLocation()
     {
         return studentLocation;
     }
-    
+
     public void setStudentLocation(String learnerLocation)
     {
         this.studentLocation = learnerLocation;
     }
+
     public int getStudentGrade()
     {
         return studentGrade;
     }
-    
+
     public void setStudentGrade(int learnerGrade)
     {
         this.studentGrade = learnerGrade;
     }
+
     public int getStudentContactInformation()
     {
         return studentContactInformation;
     }
-    
+
     public void setStudentContactInformation(int learnerContactInformation)
     {
         this.studentContactInformation = learnerContactInformation;
     }
+
     public int getStudentAge()
     {
         return studentAge;
     }
-    
+
     public void setStudentAge(int learnerAge)
     {
         this.studentAge = learnerAge;
     }
+
+//-------------------------End of getters and Setters-------------------------// 
     
-   
+
     
+//-------------------------Start of method------------------------------------//
     
-    
-    
-    
-        public void addStudent()
+    public void addStudent()
     {
 
-        
         //String used to insert the information into the database when the user pressed save details
-          String sql = "INSERT INTO StudentInformation (LastName,FirstName,StudentsClass"
-                  + ",StudentsGrade,StudentsAge,StudentsLocation,ContactInformation)" + "VALUES (?,?,?,?,?,?,?)";
-          Connection con = null;
-          try{
+        String sql = "INSERT INTO StudentInformation (LastName,FirstName,StudentsClass"
+                + ",StudentsGrade,StudentsAge,StudentsLocation,ContactInformation)" + "VALUES (?,?,?,?,?,?,?)";
+        
+        Connection con = null;
+        
+        try
+        {
 
-               con = DatabaseConnection.getConnection();
-
+            con = DatabaseConnection.getConnection();
 
             PreparedStatement statement = con.prepareStatement(sql);
 
-          
-          statement.setString(1,getLastName());
-          statement.setString(2,getFirstName());
-          statement.setString(3,getStudentClass());
-          statement.setInt(4,getStudentGrade());
-          statement.setInt(5,getStudentAge());
-          statement.setString(6,getStudentLocation());
-          statement.setInt(7,getStudentContactInformation());
-          
+            statement.setString(1, getLastName());
+            statement.setString(2, getFirstName());
+            statement.setString(3, getStudentClass());
+            statement.setInt(4, getStudentGrade());
+            statement.setInt(5, getStudentAge());
+            statement.setString(6, getStudentLocation());
+            statement.setInt(7, getStudentContactInformation());
 
-          int rows = statement.executeUpdate();
-          
-          
-          if(rows>0)
-         {
-              JOptionPane.showMessageDialog(null,"Row has been inserted");
-          }
-          
+            int rows = statement.executeUpdate();
+
+            if (rows > 0)
+            {
+                JOptionPane.showMessageDialog(null, "Row has been inserted");
+            }
+
 //          connection.close();
-          }catch(Exception e)
-          {
-             System.err.print(e);
-              JOptionPane.showMessageDialog(null,"an error occured adding your data");
-          }
-          
-    }
+        } catch (Exception e)
+        {
+            System.err.print(e);
+            JOptionPane.showMessageDialog(null, "an error occured adding your data");
+        }
 
-//    private  void writeData()
-//    {
-//        try{
-//            
-////        Connection con = DatabaseConnection.getConnection();
-//         Connection connection = DriverManager.getConnection(dataBaseConnection.dataBaseConnectionString);
-//         
-//        String sql = "INSERT INTO StudentInformation (LastName,FirstName,StudentsClass"
-//                  + ",StudentsGrade,StudentsAge,StudentsLocation,ContactInformation)" + "VALUES (?,?,?,?,?,?,?)";
-//        
-//        int rows = connection.executeUpdate();
-//        }catch(Exception e)
-//        {
-//            JOptionPane.showMessageDialog(null,"Cannot Connect");
-//        }
-//    }
+    }
+//---------------------------End of method------------------------------------//
+    
+//----------------------------Start of Method---------------------------------//
+public void addingInformationToTable()
+{
+    String populateTable = "select * from StudentInformation";
+    Connection con = null;
+    try
+    {
+       con = DatabaseConnection.getConnection();
+       
+//       PreparedStatement statement = con.prepareStatement(populateTable);
+       Statement st =  con.createStatement();
+       ResultSet rs =st.executeQuery(populateTable);
+       
+       while(rs.next())
+       {
+           String userFirstName = rs.getString("LastName");
+           String userLastName = rs.getString("FirstName");
+           String userClass = rs.getString("StudentsClass");
+           String userGrade = rs.getString("StudentsGrade");
+           String userAge = rs.getString("StudentsLocation");
+           String userContactInformation = rs.getString("ContactInformation");
+           
+           String tbData[] = {userFirstName,userLastName,userClass,userGrade,userAge,userContactInformation};
+           
+           JTable table = new JTable();
+           
+         DefaultTableModel  model = new DefaultTableModel();
+//         model.setColumnIdentifiers();
+          table.setModel(model);
+          
+          model.addRow(tbData);
+          
+       }
+    }catch(Exception e)
+    {
+       JOptionPane.showMessageDialog(null,"Could not populate Table");
+    }
+}    
+//-----------------------------End of Method----------------------------------//
 }
--- src/studentreportapplication/firstDisplay.java
+++ src/studentreportapplication/firstDisplay.java
@@ -1,75 +1,82 @@
-
 package studentreportapplication;
 
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.*;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
 //Class for if the user wants to add a new student into the system
-public class firstDisplay  extends JFrame implements ActionListener
+public class firstDisplay extends JFrame implements ActionListener
 {
-   private JTextField studentFirstName,studentLastName,studentClass,studentGrade,studentAge,
-           studentLocation,studentContactInformation;
-   private JLabel textStudentFirstName,textStudentLastName,textStudentClass,textStudentGrade,
-           textStudentAge,textStudentLocation,textStudentContactInformation;
-   
-   private JButton returnToMenu,saveDetails;
-    
-  firstDisplay()
-  {
 
-      
-        
+    private JTextField studentFirstName, studentLastName, studentClass, studentGrade, studentAge,
+            studentLocation, studentContactInformation;
+    private JLabel textStudentFirstName, textStudentLastName, textStudentClass, textStudentGrade,
+            textStudentAge, textStudentLocation, textStudentContactInformation;
+
+    private JButton returnToMenu, saveDetails;
+//   private JTable table;
+    private DefaultTableModel model;
+//   private Object[][] studentsData;
+    Students students = new Students();
+
+    firstDisplay()
+    {
+
 //-------------------Start of JButtons----------------------------------------//
- 
- //Creation of a button that will save student details to the database
- saveDetails = new JButton("Save Details");
- saveDetails.addActionListener(this);
- saveDetails.setBounds(200, 400, 200, 100);
-      
- //Creation of a menu that returns you back to the starting menu     
- returnToMenu = new JButton("Return to Menu");
- returnToMenu.addActionListener(this);
- returnToMenu.setBounds(500, 400, 200, 100);
-      
+        //Creation of a button that will save student details to the database
+        saveDetails = new JButton("Save Details");
+        saveDetails.addActionListener(this);
+        saveDetails.setBounds(250, 510, 150, 40);
+
+        //Creation of a menu that returns you back to the starting menu     
+        returnToMenu = new JButton("Return to Menu");
+        returnToMenu.addActionListener(this);
+        returnToMenu.setBounds(420, 510, 150, 40);
+
 //-------------------End of JButtons------------------------------------------//
-      
+
 //-------------------Start of TextFields--------------------------------------//
-      
-      //Creation of textfield for entering students first name
-      studentFirstName = new JTextField();
-      studentFirstName.setBounds(230, 75, 200, 25);
-      
-      //Creation of textfield for entering students last name
-      studentLastName = new JTextField();
-      studentLastName.setBounds(230, 115, 200, 25);
-      
-      //Creation of textfield for entering the students class
-      studentClass = new JTextField();
-      studentClass.setBounds(230, 155, 200, 25);
-      
-      //Creation of textfield for entering the students grade
-      studentGrade = new JTextField();
-      studentGrade.setBounds(970, 75, 200, 25);
-      
-      //Creation of textField for entering the students age
-      studentAge = new JTextField();
-      studentAge.setBounds(970, 115, 200, 25);
-      
-      //Creation of textField for entering the students location
-      studentLocation = new JTextField();
-      studentLocation.setBounds(970, 155, 200, 25);
-      
-      //Creation of textField for entering the students contact information
-      studentContactInformation = new JTextField();
-      studentContactInformation.setBounds(970,196, 200, 25);
-      
-      
+        //Creation of textfield for entering students first name
+        studentFirstName = new JTextField();
+        studentFirstName.setBounds(230, 75, 200, 25);
+
+        //Creation of textfield for entering students last name
+        studentLastName = new JTextField();
+        studentLastName.setBounds(230, 115, 200, 25);
+
+        //Creation of textfield for entering the students class
+        studentClass = new JTextField();
+        studentClass.setBounds(230, 155, 200, 25);
+
+        //Creation of textfield for entering the students grade
+        studentGrade = new JTextField();
+        studentGrade.setBounds(970, 75, 200, 25);
+
+        //Creation of textField for entering the students age
+        studentAge = new JTextField();
+        studentAge.setBounds(970, 115, 200, 25);
+
+        //Creation of textField for entering the students location
+        studentLocation = new JTextField();
+        studentLocation.setBounds(970, 155, 200, 25);
+
+        //Creation of textField for entering the students contact information
+        studentContactInformation = new JTextField();
+        studentContactInformation.setBounds(970, 196, 200, 25);
+
 //-----------------End of TextFields------------------------------------------//
 
 //-----------------start of Labels--------------------------------------------//
@@ -79,63 +86,82 @@ public class firstDisplay  extends JFrame implements ActionListener
         textStudentInformation.setFont(new Font("gotham", Font.PLAIN, 20));
         textStudentInformation.setBounds(520, 0, 300, 50);
         textStudentInformation.setText("Student Information");
-        
+
         //Creation of label to label students first name textfield
         textStudentFirstName = new JLabel();
-        textStudentFirstName.setFont(new Font("gotham",Font.PLAIN,20));//Remove Bold
+        textStudentFirstName.setFont(new Font("gotham", Font.PLAIN, 20));//Remove Bold
         textStudentFirstName.setBounds(120, 60, 200, 50);
         textStudentFirstName.setText("First Name:");
         textStudentFirstName.setAlignmentX(RIGHT_ALIGNMENT);
-        
+
         //Creation of label that labels the lastname textfield as "Last Name"
         textStudentLastName = new JLabel();
         textStudentLastName.setFont(new Font("gotham", Font.PLAIN, 20));
         textStudentLastName.setBounds(120, 100, 140, 50);
         textStudentLastName.setText("Last Name:");
-        
-        
+
         //Creation of label that labels the studentClass textfield as "Student Class"
         textStudentClass = new JLabel();
         textStudentClass.setFont(new Font("gotham", Font.PLAIN, 20));
         textStudentClass.setBounds(82, 135, 160, 60);
         textStudentClass.setText("Students Class:");
         textStudentClass.setAlignmentX(Component.RIGHT_ALIGNMENT);
-        
+
         //Creation of lable that labels the studentGrade textfield as "Grade"
         textStudentGrade = new JLabel();
         textStudentGrade.setFont(new Font("gotham", Font.PLAIN, 20));
         textStudentGrade.setBounds(820, 60, 300, 50);
         textStudentGrade.setText("Students Grade:");
         textStudentGrade.setAlignmentX(Component.RIGHT_ALIGNMENT);
-        
+
         //Creation of label that labels the studentAge textfield as "Students Age"
         textStudentAge = new JLabel();
         textStudentAge.setFont(new Font("gotham", Font.PLAIN, 20));
         textStudentAge.setBounds(840, 100, 300, 50);
         textStudentAge.setText("Students Age:");
         textStudentAge.setAlignmentX(Component.RIGHT_ALIGNMENT);
-        
+
         //Creation of label that labels the studentLocation textfield as "Students Location"
         textStudentLocation = new JLabel();
         textStudentLocation.setFont(new Font("gotham", Font.PLAIN, 20));
         textStudentLocation.setBounds(800, 140, 300, 50);
         textStudentLocation.setText("Students Location:");
         textStudentLocation.setAlignmentX(Component.RIGHT_ALIGNMENT);
-        
+
         //Creation of label that labels the studentContactInformation textfield as "Contact Information"
         textStudentContactInformation = new JLabel();
         textStudentContactInformation.setFont(new Font("gotham", Font.PLAIN, 20));
         textStudentContactInformation.setBounds(784, 181, 300, 50);
         textStudentContactInformation.setText("Contact Information:");
         textStudentContactInformation.setAlignmentX(Component.RIGHT_ALIGNMENT);
-        
 
 //-------------------End of labels--------------------------------------------//
-      
-      
-      
-//----------------Start of JFRAME---------------------------------------------//
 
+//------------------Start of creation of table--------------------------------//
+        String[] columnNames =
+        {
+            "First Name", "Last Name", "Students Class",
+            "Students Grade", "Student Age", "Studet Location", "Contact Information"
+        };
+
+        JTable table = new JTable();
+
+        model = new DefaultTableModel();
+        model.setColumnIdentifiers(columnNames);
+
+        table.setBounds(400, 300, 400, 300);
+
+        table.setModel(model);
+        table.setBackground(Color.WHITE);
+        table.setRowHeight(30);
+
+        JScrollPane jScrollPane = new JScrollPane(table);
+
+        jScrollPane.setBounds(250, 300, 800, 200);
+
+//------------------End of creation of table----------------------------------//
+
+//----------------Start of JFRAME---------------------------------------------//
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
         this.setSize(1400, 700);
@@ -157,97 +183,66 @@ public class firstDisplay  extends JFrame implements ActionListener
         this.add(textStudentInformation);
         this.add(returnToMenu);
         this.add(saveDetails);
-        
+//        this.add(table);
+        this.add(jScrollPane);
+
 //-----------------End of JFRAME----------------------------------------------//
-  }
-    
-    
-    
-    
-    
-    
-    
-    
+    }
+
     @Override
     public void actionPerformed(ActionEvent e)
     {
-       if(e.getSource() == returnToMenu)
-       {
-           new menu();
-           this.setVisible(false);
-       }
-       
-       if(e.getSource() == saveDetails)
-       {
-           int learnersAge = Integer.parseInt(studentAge.getText());
-           
-           int learnersGrade = Integer.parseInt(studentGrade.getText());
-          
-           int learnersContactInformation = Integer.parseInt(studentContactInformation.getText());
-//           addingToDataBase();
-             Students students = new Students();
-             students.setLastName(studentLastName.getText());
-             students.setFirstName(studentFirstName.getText());
-             students.setStudentAge(learnersAge);
-             students.setStudentClass(studentClass.getText());
-             students.setStudentLocation(studentLocation.getText());
-             students.setStudentContactInformation(learnersContactInformation);
-             students.setStudentGrade(learnersGrade);
-             
-             students.addStudent();
-             
-            
-       }
-    }
-    
-    
-    
-//--------------------------Start of method-----------------------------------//
-    
-//public void addingToDataBase()
-//{
-    
-//    String dataBaseURL = "jdbc:sqlserver://DESKTOP-87VQUG8:56410;databaseName=StudentPortal;"
-//              + "encrypt=true;trustServerCertificate=true;integratedSecurity=true";
-      
-//      try
-//      {
-         //Creation of connection to the database
-//          Connection connection = DriverManager.getConnection(dataBaseURL);
-          
+        if (e.getSource() == returnToMenu)
+        {
+            new menu();
+            this.setVisible(false);
+        }
 
-          
-          //String used to insert the information into the database when the user pressed save details
-//          String sql = "INSERT INTO StudentInformation (LastName,FirstName,StudentsClass"
-//                  + ",StudentsGrade,StudentsAge,StudentsLocation,ContactInformation)" + "VALUES (?,?,?,?,?,?,?)";
-//          
-//          
-//          PreparedStatement statement = connection.prepareStatement(sql);
-//          
-//          statement.setString(1,studentLastName.getText());
-//          statement.setString(2,studentFirstName.getText());
-//          statement.setString(3,studentClass.getText());
-//          statement.setString(4,studentGrade.getText());
-//          statement.setString(5,studentAge.getText());
-//          statement.setString(6,studentLocation.getText());
-//          statement.setString(7,studentContactInformation.getText());
-//          
-//          
-//          int rows = statement.executeUpdate();
-//          
-//          if(rows>0)
-//          {
-//              JOptionPane.showMessageDialog(null,"Row has been inserted");
-//          }
-//          
-//          connection.close();
-//          
-//      }catch(Exception e)
-//      {
-//        JOptionPane.showMessageDialog(null,"Connection Failed");
-//        e.printStackTrace();
-//      }
-//}
- 
-//--------------------------End of method-------------------------------------//
+        if (e.getSource() == saveDetails)
+        {
+            int learnersAge = Integer.parseInt(studentAge.getText());
+
+            int learnersGrade = Integer.parseInt(studentGrade.getText());
+
+            int learnersContactInformation = Integer.parseInt(studentContactInformation.getText());
+
+            students.setLastName(studentLastName.getText());
+
+            students.setFirstName(studentFirstName.getText());
+
+            students.setStudentAge(learnersAge);
+
+            students.setStudentClass(studentClass.getText());
+
+            students.setStudentLocation(studentLocation.getText());
+
+            students.setStudentContactInformation(learnersContactInformation);
+
+            students.setStudentGrade(learnersGrade);
+
+            Object[] row = new Object[7];
+
+            row[0] = studentFirstName.getText();
+            row[1] = studentLastName.getText();
+            row[2] = studentClass.getText();
+            row[3] = studentGrade.getText();
+            row[4] = studentAge.getText();
+            row[5] = studentLocation.getText();
+            row[6] = studentContactInformation.getText();
+            model.addRow(row);
+//            students.addingInformationToTable();
+            students.addStudent();
+//            Connection con = null;
+//            try{
+//            con = DatabaseConnection.getConnection();
+//            con.close();
+//            }catch(Exception b)
+//            {
+//                JOptionPane.showMessageDialog(null,"Cant Save Data");
+//            }
+////             model.addRow(new Object[] {studentFirstName.getText(),studentFirstName.getText(),studentClass.getText()
+////             ,learnersGrade,learnersAge,studentLocation.getText(),learnersContactInformation});
+        }
+    }
+
 }
